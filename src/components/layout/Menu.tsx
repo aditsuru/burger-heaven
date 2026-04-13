2ba03fc167d9cd174bdbe7bd1f2c70a30a0da72b
Adi <me@aditsuru.com>
2026-04-13 13:20:35 +0530

    reduce menu items size

--- src/components/layout/Menu.tsx
+++ src/components/layout/Menu.tsx
@@ -19,12 +19,12 @@ const menuItems = [
 
 function Menu() {
   return (
-    <div className="mt-16 grid w-full gap-8 px-16 lg:mt-16 lg:grid-cols-2">
+    <div className="mt-16 grid w-full gap-8 px-16 lg:mt-16 lg:grid-cols-2 place-items-center">
       {menuItems.map((item) => (
         <AspectRatio
           key={item.alt}
           ratio={650 / 753}
-          className="relative col-span-1 rounded-4xl"
+          className="relative col-span-1 rounded-4xl w-xl"
         >
           <img src={item.src} alt={item.alt} className="h-full object-cover" />
           <div className="absolute bottom-0 left-0 flex w-full flex-col items-start justify-center rounded-t-4xl bg-background p-4 font-bowlby text-foreground lg:bottom-10 lg:left-10 lg:w-[70%] lg:rounded-4xl lg:p-8">
